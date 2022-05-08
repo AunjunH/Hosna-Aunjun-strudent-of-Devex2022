1fa43cd5a684f83bfa0364ecbc0b3674de300843
AunjunH <mizanheera@gmail.com>
2022-05-07 21:39:25 -0500

    yes

---
+++ src/com/java/Projectworkhomework/EveOddMethod.java
@@ -0,0 +1,15 @@
+package com.java.Projectworkhomework;
+
+public class EveOddMethod {
+    public static void main(String[] args) {
+evenOrOdd(50);
+
+    }
+        static void  evenOrOdd(int x){
+            if (x % 2 == 0) {
+                System.out.println("the number is " + "even");
+            } else {
+                System.out.println("the number is " + "odd");
+            }
+        }
+    }
---
+++ src/com/java/Projectworkhomework/hW12.java
@@ -0,0 +1,14 @@
+package com.java.Projectworkhomework;
+// Write a method which apply in main method
+public class hW12 {
+    public static void main(String[] args) {
+       myvoice();
+    }
+    static void  myvoice(){
+        System.out.println("i am hosna");
+
+
+
+    }
+}
+
